3cf48306aed1a0dbb0508026d9f2ab96853ec107
S.A.Nandhini <nandhinisa06@gmail.com>
2026-07-23 21:37:07 +0530

    Add dark mode functionality

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const button = document.querySelector("#darkModeBtn");
 button.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
 });
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+});
