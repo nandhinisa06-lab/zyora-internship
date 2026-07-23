58e6a7b56d312a884b99f78224cd6b7b9f1acaed
S.A.Nandhini <nandhinisa06@gmail.com>
2026-07-23 21:41:22 +0530

    Add contact form validation

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ const darkModeBtn = document.getElementById("darkModeBtn");
 darkModeBtn.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
 });
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill all fields.");
+        return;
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailPattern.test(email)) {
+        alert("Enter a valid email.");
+        return;
+    }
+
+    alert("Form submitted successfully!");
+});
