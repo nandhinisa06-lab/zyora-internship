b2152ff5919ed3ada18942676c04176006af984c
S.A.Nandhini <nandhinisa06@gmail.com>
2026-07-24 07:59:21 +0530

    Fix dark mode toggle

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-alert("JavaScript Loaded!");
-// Dark Mode Toggle
-const button = document.querySelector("#darkModeBtn");
-
-button.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
-});
 const darkModeBtn = document.getElementById("darkModeBtn");
 
 darkModeBtn.addEventListener("click", function () {
