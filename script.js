acd66527810eb295a080515594eb44c4846f32a9
S.A.Nandhini <nandhinisa06@gmail.com>
2026-07-31 19:18:24 +0530

    Fixed weather API fetch and loading states

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 document.getElementById("searchBtn").addEventListener("click", function () {
-    alert("Button Working!");
+
+    const city = document.getElementById("cityInput").value.trim();
+
+    if (city === "") {
+        alert("Please enter a city name!");
+        return;
+    }
+
+    document.getElementById("cityName").textContent = city;
+    document.getElementById("temperature").textContent = "Loading...";
+    document.getElementById("wind").textContent = "Loading...";
+
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=13.0827&longitude=80.2707&current=temperature_2m,wind_speed_10m")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("temperature").textContent =
+                data.current.temperature_2m + "°C";
+
+            document.getElementById("wind").textContent =
+                data.current.wind_speed_10m + " km/h";
+        })
+        .catch(() => {
+            document.getElementById("temperature").textContent = "Error";
+            document.getElementById("wind").textContent = "Error";
+        });
+
 });
